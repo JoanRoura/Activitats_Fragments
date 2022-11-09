48fc52342a385fb3e7372517ba711e66f32c5272
Joan Roura <joan.roura@institutvidreres.cat>
2022-11-09 13:30:05 +0100

    Projecte Inicial

---
+++ app/src/main/java/com/example/projecte_fragments/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.projecte_fragments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Spinner
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navController = this.findNavController(R.id.myNavHostFragment)
+
+        NavigationUI.setupActionBarWithNavController(this,navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
+    }
+}
